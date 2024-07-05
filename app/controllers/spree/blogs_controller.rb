db9d973ba8cd8904399c973bc9fa2c5c918e1b8b
byideas <info@burstyourideas.com>
2024-07-05 08:06:43 +0200

    fixed by  zeitwerk:check spree:admin:storeController

--- app/controllers/spree/blogs_controller.rb
+++ app/controllers/spree/blogs_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Spree::BlogsController < Spree::StoresController
+class Spree::BlogsController < Spree::Api::V1::StoresController
   before_action :init_pagination, only: %i[show tag]
 
   def show
