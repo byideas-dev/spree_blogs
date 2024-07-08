317ea694a1fdb94eaf82c6b30ce995e0220153cd
byideas <info@burstyourideas.com>
2024-07-08 09:22:29 +0200

    fix delete

---
+++ app/controllers/spree/blogs_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class Spree::BlogsController < Spree::BaseController
+  before_action :init_pagination, only: %i[show tag]
+
+  def show
+    set_blog(:id)
+
+    @posts = @blog.posts.published_and_visible.page(@pagination_page).per(@pagination_per_page)
+
+    @title = if @blog.meta_title.present?
+      @blog.meta_title
+    else
+      @blog.title
+    end
+  end
+
+  def tag
+    set_blog(:blog_id)
+
+    @tag = MakeTaggable::Tag.friendly.find(params[:tag])
+
+    @posts = @blog.posts.published_and_visible.by_tag(@tag).page(@pagination_page).per(@pagination_per_page)
+
+    @title = "#{@blog.title} | #{@tag.to_s.titlecase}"
+  end
+
+  private
+
+  def set_blog(arg)
+    @blog = Spree::Blog.by_store(current_store).friendly.find(params[arg])
+  end
+
+  def init_pagination
+    @pagination_page = params[:page].present? ? params[:page].to_i : 1
+    @pagination_per_page = params[:per_page].present? ? params[:per_page].to_i : 8
+  end
+end
---
+++ app/controllers/spree/posts_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Spree::PostsController < Spree::StoresController
+  def show
+    @blog = Spree::Blog.by_store(current_store).friendly.find(params[:blog_id])
+
+    @post = if try_spree_current_user.try(:has_spree_role?, "admin")
+      @blog.posts.find(params[:id])
+    else
+      @blog.posts.published_and_visible.find(params[:id])
+    end
+
+    @title = @post.post_seo_title
+
+    respond_to do |format|
+      format.html
+      format.rss { render layout: false }
+    end
+  end
+end
